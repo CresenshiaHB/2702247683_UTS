4a48ff4e94ebcbe14339523f37efe23d427123be
CresenshiaHB <cresenshiahillary05@gmail.com>
2025-04-19 23:28:43 +0700

    Add files via upload

---
+++ 2702247683_OOP.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Nama : Cresenshia Hillary Benida
+# 
+# NIM : 2702247683
+
+# # Dataset A (Loan)
+# Mengenai dataset:
+# 
+# Dataset A terdiri dari 14 kolom dan 45000 baris, berikut adalah penjelasan dari setiap kolom
+# 
+# - person_age = Usia dari orang tersebut
+# - person_gender = Gender dari orang tersebut
+# - person_education = Tingkat pendidikan tertinggi
+# - person_income = Pendapatan tahunan
+# - person_emp_exp = Tahun pengalaman bekerja
+# - person_home_ownership = Status kepemilikan tempat huni
+# - loan_amnt = Jumlah pinjaman yang diminta
+# - loan_intent = Tujuan dari pinjaman
+# - loan_int_rate = Suku bunga pinjaman
+# - loan_percent_income = Jumlah pinjaman sebagai persentase dari pendapatan tahunan
+# - cb_person_cred_hist_length = Durasi kredit dalam tahun
+# - credit_score = Skor kredit dari orang tersebut
+# - previous_loan_defaults_on_file = Indikator tunggakan pinjaman sebelumnya
+# - loan_status (target variable) = Persetujuan pinjaman; 1: diterima dan 0: ditolak
+
+# ## OOP
+# Seluruh proses training dari algoritma machine learning yang terbaik dibubah dalam format OOP
+
+# In[1]:
+
+
+# Nama : Cresenshia Hillary Benida
+# NIM  : 2702247683
+
+# import library
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+import pickle
+
+# class DataHandler 
+class DataHandler:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data = None
+        self.input_df = None  
+        self.output_df = None
+
+    # self data read csv
+    def load_data(self):
+        self.data = pd.read_csv(self.file_path, delimiter=',')
+
+    def remove_column(self, column):
+        self.data = self.data.drop(columns=[column])
+    
+    # data target column
+    def create_input_output(self, target_column):
+        self.output_df = self.data[target_column]
+        self.input_df = self.data.drop(target_column, axis=1)
+
+    # Handle value inkonsisten
+    def replace_values(self, column_name, mapping_value):
+        if column_name in self.data.columns:
+            self.data[column_name] = self.data[column_name].astype(str).str.strip().str.lower()     # seragamkan dulu valuenya
+            self.data[column_name] = self.data[column_name].replace(mapping_value)
+
+# class ModelHandler
+class ModelHandler:
+    def __init__(self, input_data, output_data):
+        self.input_data = input_data
+        self.output_data = output_data
+        self.createModel()
+        self.x_train, self.x_test, self.y_train, self.y_test, self.y_predict = [None] * 5
+
+    # Splitting Data menjadi train dan test data dengan proporsi 80:20
+    def split_data(self, test_size=0.2, random_state=42):
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
+           self.input_data, self.output_data, test_size=test_size, random_state=random_state)
+        
+    # Check Outliers dengan Boxplot
+    def checkTheOutliersWithBox(self, kolom):
+        boxplot = self.x_train.boxplot(column=[kolom]) 
+        plt.show()
+
+    # Function untuk mengembalikan nilai outliers (digunakan untuk mengisi missing value)
+    def check_outliers(self, column):
+        return abs(self.x_train[column].skew()) > 1
+      
+    # Check & Handling missing data
+    def checkMissingValues(self, column):
+        return self.x_train[column].isnull().sum() > 0
+
+    # Mengisi nilai missing value dengan 3 kondisi dan 3 cara handling yang berbeda
+    # Kondisi 1 : missing value di kolom person_income -> isi dengan value pembagian loan_amnt / loan_percent_income
+    # Kondisi 2 : missing value di kolom dengan outliers -> isi dengan nilai median train data
+    # Konsisi 3 : missing value di kolom tanpa outliers -> isi dengan nilai mean train data
+    def fillingNAWithNumbers(self, column):
+        if column == 'person_income':
+            # Imput value berdasarkan column loan_amnt dan loan_percent_income
+            missing_row_income = self.x_train[column].isna()
+            self.x_train.loc[missing_row_income, column] = (
+                self.x_train.loc[missing_row_income, 'loan_amnt'] /
+                self.x_train.loc[missing_row_income, 'loan_percent_income']
+            )
+            missing_row_income_2 = self.x_test[column].isna()
+            self.x_test.loc[missing_row_income_2, column] = (
+                self.x_test.loc[missing_row_income_2, 'loan_amnt'] /
+                self.x_test.loc[missing_row_income_2, 'loan_percent_income']
+            )
+            # handle jika ada nilai pembagi yang 0 sehingga menghasilkan value person_income infinite
+            self.x_train[column].replace([np.inf, -np.inf], np.nan, inplace=True)
+            self.x_test[column].replace([np.inf, -np.inf], np.nan, inplace=True)
+            median_income_train = self.x_train[column].median()
+            median_income_test = self.x_test[column].median()
+            self.x_train[column].fillna(median_income_train, inplace=True)
+            self.x_test[column].fillna(median_income_test, inplace=True)
+        else:
+            if self.check_outliers(column):
+                fill_value = self.x_train[column].median()
+            else:
+                fill_value = self.x_train[column].mean()
+            self.x_train[column].fillna(fill_value, inplace=True)
+            self.x_test[column].fillna(fill_value, inplace=True)
+
+    def fillingNAWithCategory(self, column_name):
+        mode_value = self.x_train[column_name].mode()[0]
+        self.x_train[column_name].fillna(mode_value, inplace=True)
+        self.x_test[column_name].fillna(mode_value, inplace=True)
+
+    def encode_columns(self,
+        binary_columns: dict = None,
+        label_columns: dict = None,
+        one_hot_columns: list = None):
+        # Binary encoding
+        if binary_columns:
+            self.x_train = self.x_train.replace(binary_columns)
+            self.x_test = self.x_test.replace(binary_columns)
+        # Label encoding
+        if label_columns:
+            for col, mapping in label_columns.items():
+                self.x_train[col] = self.x_train[col].map(mapping)
+                self.x_test[col] = self.x_test[col].map(mapping)
+        # One-hot encoding
+        if one_hot_columns:
+            for col in one_hot_columns:
+                encoder = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
+                encode_train = pd.DataFrame(encoder.fit_transform(self.x_train[[col]]), columns=encoder.get_feature_names_out([col]))
+                encode_test = pd.DataFrame(encoder.fit_transform(self.x_test[[col]]), columns=encoder.get_feature_names_out([col]))
+                encode_train.index = self.x_train.index
+                encode_test.index = self.x_test.index
+                self.x_train = pd.concat([self.x_train.drop(columns=[col]), encode_train], axis=1)
+                self.x_test = pd.concat([self.x_test.drop(columns=[col]), encode_test], axis=1)
+        # memastikan semua kolom categorical berubah menjadi numerical
+        self.x_train = self.x_train.apply(pd.to_numeric, errors='raise')
+        self.x_test = self.x_test.apply(pd.to_numeric, errors='raise')
+
+    def createModel(self):
+         self.model = xgb.XGBClassifier()
+
+    def train_model(self):
+        self.model.fit(self.x_train, self.y_train)
+
+    def evaluate_model(self):
+        predictions = self.model.predict(self.x_test)
+        return accuracy_score(self.y_test, predictions)
+    
+    def makePrediction(self):
+        self.y_predict = self.model.predict(self.x_test) 
+        
+    def createReport(self):
+        print('\nClassification Report\n')
+        print(classification_report(self.y_test, self.y_predict))
+
+    def save_model_to_file(self, filename):
+        with open(filename, 'wb') as file: 
+            pickle.dump(self.model, file) 
+
+
+
+file_path = 'Dataset_A_loan.csv'  
+data_handler = DataHandler(file_path)
+data_handler.load_data()
+
+mapping_value_gender = {
+    'fe male': 'female',
+    'female': 'female'
+}
+data_handler.replace_values('person_gender', mapping_value_gender)
+
+data_handler.create_input_output('loan_status')
+input_df = data_handler.input_df
+output_df = data_handler.output_df
+
+model_handler = ModelHandler(input_df, output_df)
+model_handler.split_data()
+
+for column_name in input_df.columns:
+    # menampilkan boxplot untuk column numerical saja
+    if input_df[column_name].dtype in ['int64', 'float64']:
+        model_handler.checkTheOutliersWithBox(column_name)
+    # Check dan isi missing values
+    if model_handler.checkMissingValues(column_name):
+        # numerical column
+        if input_df[column_name].dtype in ['int64', 'float64']:
+            model_handler.fillingNAWithNumbers(column_name)  
+        # categprical column
+        else:
+            model_handler.fillingNAWithCategory(column_name) 
+
+model_handler.encode_columns(
+    binary_columns={
+        'person_gender': {'male': 1, 'female': 0},
+        'previous_loan_defaults_on_file': {'Yes': 1, 'No': 0}
+    },
+    label_columns={
+        'person_education': {
+            'high school': 0,
+            'associate': 1,
+            'bachelor': 2,
+            'master': 3,
+            'doctorate': 4
+        }
+    },
+    one_hot_columns=['person_home_ownership', 'loan_intent']
+)
+
+print("XGBoost Model")
+model_handler.train_model()
+
+model_handler.makePrediction()
+model_handler.createReport()
+#model_handler.save_model_to_file('xgb_class.pkl') 
+
---
+++ 2702247683_inference.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Nama : Cresenshia Hillary Benida
+# 
+# NIM : 2702247683
+
+# # Dataset A (Loan)
+# 
+# Mengenai dataset:
+# 
+# Dataset A terdiri dari 14 kolom dan 45000 baris, berikut adalah penjelasan dari setiap kolom
+# 
+# - person_age = Usia dari orang tersebut
+# - person_gender = Gender dari orang tersebut
+# - person_education = Tingkat pendidikan tertinggi
+# - person_income = Pendapatan tahunan
+# - person_emp_exp = Tahun pengalaman bekerja
+# - person_home_ownership = Status kepemilikan tempat huni
+# - loan_amnt = Jumlah pinjaman yang diminta
+# - loan_intent = Tujuan dari pinjaman
+# - loan_int_rate = Suku bunga pinjaman
+# - loan_percent_income = Jumlah pinjaman sebagai persentase dari pendapatan tahunan
+# - cb_person_cred_hist_length = Durasi kredit dalam tahun
+# - credit_score = Skor kredit dari orang tersebut
+# - previous_loan_defaults_on_file = Indikator tunggakan pinjaman sebelumnya
+# - loan_status (target variable) = Persetujuan pinjaman; 1: diterima dan 0: ditolak
+
+# ## Inference / Prediction Code
+# Membuat code inference/prediction untuk proses deployment 
+
+# In[1]:
+
+
+import streamlit as st
+import joblib
+import numpy as np
+
+# Load the machine learning model
+model = joblib.load('xgb_class.pkl')
+
+# Encoding untuk input categorical features
+def encode_input(data):
+    # Binary encoding
+    gender_map = {'male': 1, 'female': 0}
+    previous_defaults_map = {'Yes': 1, 'No': 0}
+    data['person_gender'] = gender_map.get(data['person_gender'], 0)
+    data['previous_loan_defaults_on_file'] = previous_defaults_map.get(data['previous_loan_defaults_on_file'], 0)
+    
+    # Label encoding
+    education_map = {
+        'High School': 0,
+        'Associate': 1,
+        'Bachelor': 2,
+        'Master': 3,
+        'Doctorate': 4
+    }
+    data['person_education'] = education_map.get(data['person_education'], 0)
+    
+    # One-hot encoding
+    home_ownership_categories = ['MORTGAGE', 'RENT', 'OWN', 'OTHER']
+    loan_intent_categories = ['PERSONAL', 'VENTURE', 'EDUCATION', 'HOMEIMPROVEMENT', 'MEDICAL', 'DEBTCONSOLIDATION']
+    for cat in home_ownership_categories:
+        data[f'person_home_ownership_{cat}'] = 1 if data['person_home_ownership'] == cat else 0
+    for cat in loan_intent_categories:
+        data[f'loan_intent_{cat}'] = 1 if data['loan_intent'] == cat else 0
+    del data['person_home_ownership']
+    del data['loan_intent']
+    
+    return data
+
+def make_prediction(features):
+    input_array = np.array(features).reshape(1, -1)
+    prediction = model.predict(input_array)
+    return prediction[0]
+
+def main():
+    st.title('Loan Problem Machine Learning Model Deployment')
+
+    # Input user per fitur
+    person_age = st.number_input('Age', min_value=0, value=25)
+    person_gender = st.selectbox('Gender', ['male', 'female'])
+    person_education = st.selectbox('Education Level', ['High School', 'Associate', 'Bachelor', 'Master', 'Doctorate'])
+    person_income = st.number_input('Annual Income', min_value=0, value=30000)
+    person_emp_exp = st.number_input('Employment Experience (years)', min_value=0, value=1)
+    person_home_ownership = st.selectbox('Home Ownership', ['MORTGAGE', 'RENT', 'OWN', 'OTHER'])
+    loan_intent = st.selectbox('Loan Intent', ['PERSONAL', 'VENTURE', 'EDUCATION', 'HOMEIMPROVEMENT', 'MEDICAL', 'DEBTCONSOLIDATION'])
+    loan_int_rate = st.number_input('Loan Interest Rate (%)', value=10.0)
+    loan_percent_income = st.number_input('Loan Percent of Income', value=0.1)
+    cb_person_cred_hist_length = st.number_input('Credit History Length (years)', min_value=0, value=3)
+    credit_score = st.number_input('Credit Score', min_value=0, value=600)
+    previous_loan_defaults_on_file = st.selectbox('Previous Loan Defaults', ['Yes', 'No'])
+
+    if st.button('Make Prediction'):
+        # Input dari user 
+        features = {
+            'person_age': person_age,
+            'person_gender': person_gender,
+            'person_education': person_education,
+            'person_income': person_income,
+            'person_emp_exp': person_emp_exp,
+            'person_home_ownership': person_home_ownership,
+            'loan_amnt': loan_amnt,
+            'loan_intent': loan_intent,
+            'loan_int_rate': loan_int_rate,
+            'loan_percent_income': loan_percent_income,
+            'cb_person_cred_hist_length': cb_person_cred_hist_length,
+            'credit_score': credit_score,
+            'previous_loan_defaults_on_file': previous_loan_defaults_on_file
+        }
+
+        # Encode fitur kategorikal dan one-hot
+        encoded_features = encode_input(features)
+
+        # Urutkan fitur sesuai urutan yang dipakai oleh model
+        feature_order = [
+            'person_age',
+            'person_gender',
+            'person_education',
+            'person_income',
+            'person_emp_exp',
+            'loan_amnt',
+            'loan_int_rate',
+            'loan_percent_income',
+            'cb_person_cred_hist_length',
+            'credit_score',
+            'previous_loan_defaults_on_file',
+            'person_home_ownership_RENT',
+            'person_home_ownership_OWN',
+            'person_home_ownership_MORTGAGE',
+            'loan_intent_PERSONAL',
+            'loan_intent_EDUCATION',
+            'loan_intent_MEDICAL'
+        ]
+
+        # Encode nilai features dari user sebelum dimasukan ke dalam model
+        features = [encoded_features[feat] for feat in feature_order]
+
+        # Prediksi
+        result = make_prediction(features)
+
+        st.success(f'The prediction (loan status) is: {"Approved" if result == 1 else "Rejected"}')
+
+if __name__ == '__main__':
+    main()
+
